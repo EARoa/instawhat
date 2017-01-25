c49f51e8f57b6813b7f29851e0f88fef19880909
Efrain Ayllon <jesusayllonroa@gmail.com>
2017-01-25 09:23:56 -0600

    working

--- InstaWhat/ViewController.swift
+++ InstaWhat/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import Sharaku
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var showSharokuButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func sharakuButtonTapped(_ sender: Any) {
+        print("hello!")
+        let image = UIImage(named: "sample")
+        let vc = SHViewController(image: image!)
+        vc.delegate = self
+        present(vc, animated: true, completion: nil)
+    }
 }
 
+
+
+extension ViewController: SHViewControllerDelegate {
+    func shViewControllerImageDidFilter(image: UIImage) {
+        imageView.image = image
+        showSharokuButton.isHidden = true
+    }
+    
+    func shViewControllerDidCancel() {
+    }
+}
